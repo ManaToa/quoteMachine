c4b1a054b186de90ca02c6249e358f9d91b6dcbb
Manatoa <43567527+ManaToa@users.noreply.github.com>
2024-03-01 13:20:12 +0100

    Create quotes.ts

---
+++ quotes.ts
@@ -0,0 +1,160 @@
+export interface QuoteType {
+  quote: string
+  author: string
+}
+
+const quoteList: QuoteType[] = [
+  {
+    quote: `The greatest glory in living lies not in never falling, but in rising every time we fall.`,
+    author: `Nelson Mandela`,
+  },
+  {
+    quote: `The way to get started is to quit talking and begin doing.`,
+    author: `Walt Disney`,
+  },
+  {
+    quote: `Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.`,
+    author: `Steve Jobs`,
+  },
+  {
+    quote: `The future belongs to those who believe in the beauty of their dreams.`,
+    author: `Eleanor Roosevelt`,
+  },
+  {
+    quote: `If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.`,
+    author: `Oprah Winfrey`,
+  },
+  {
+    quote: `If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.`,
+    author: `James Cameron`,
+  },
+  {
+    quote: `You may say I'm a dreamer, but I'm not the only one. I hope someday you'll join us. And the world will live as one.`,
+    author: `John Lennon`,
+  },
+  {
+    quote: `You must be the change you wish to see in the world.`,
+    author: `Mahatma Gandhi`,
+  },
+  {
+    quote: `Spread love everywhere you go. Let no one ever come to you without leaving happier.`,
+    author: `Mother Teresa`,
+  },
+  {
+    quote: `The only thing we have to fear is fear itself.`,
+    author: `Franklin D. Roosevelt`,
+  },
+  {
+    quote: `Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.`,
+    author: `Martin Luther King Jr.`,
+  },
+  {
+    quote: `Well done is better than well said.`,
+    author: `Benjamin Franklin`,
+  },
+  {
+    quote: `Do one thing every day that scares you.`,
+    author: `Eleanor Roosevelt`,
+  },
+  {
+    quote: `The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.`,
+    author: `Helen Keller`,
+  },
+
+  {
+    quote: `It is during our darkest moments that we must focus to see the light.`,
+    author: `Aristotle`,
+  },
+
+  {
+    quote: `Do not go where the path may lead, go instead where there is no path and leave a trail.`,
+    author: `Ralph Waldo Emerson`,
+  },
+
+  {
+    quote: `Be yourself; everyone else is already taken.`,
+    author: `Oscar Wilde`,
+  },
+  {
+    quote: `Life is a journey that must be traveled no matter how bad the roads and accommodations.`,
+    author: `Oliver Goldsmith`,
+  },
+  {
+    quote: `The greatest pleasure in life is doing what people say you cannot do.`,
+    author: `Walter Bagehot`,
+  },
+  {
+    quote: `Life is like a coin. You can spend it any way you wish, but you only spend it once.`,
+    author: `Lillian Dickson`,
+  },
+  {
+    quote: `Life is made of ever so many partings welded together.`,
+    author: `Charles Dickens`,
+  },
+  {
+    quote: `Life is what happens to us while we are making other plans.`,
+    author: `Allen Saunders`,
+  },
+  {
+    quote: `Life is really simple, but men insist on making it complicated.`,
+    author: `Confucius`,
+  },
+  {
+    quote: `In the end, it's not the years in your life that count. It's the life in your years.`,
+    author: `Abraham Lincoln`,
+  },
+  {
+    quote: `Life isn't about finding yourself. Life is about creating yourself.`,
+    author: `George Bernard Shaw`,
+  },
+  {
+    quote: `The purpose of life is not to be happy. It is to be useful, to be honorable, to be compassionate, to have it make some difference that you have lived and lived well.`,
+    author: `Ralph Waldo Emerson`,
+  },
+  {
+    quote: `I have found that if you love life, life will love you back.`,
+    author: `Arthur Rubinstein`,
+  },
+  {
+    quote: `Life is short, and it's up to you to make it sweet.`,
+    author: `Sarah Louise Delany`,
+  },
+  {
+    quote: `Life doesn't require that we be the best, only that we try our best.`,
+    author: `H. Jackson Brown Jr.`,
+  },
+  {
+    quote: `I enjoy life when things are happening. I don't care if it's good things or bad things. That means you're alive.`,
+    author: `Joan Rivers`,
+  },
+  {
+    quote: `To live is the rarest thing in the world. Most people exist, that is all.`,
+    author: `Oscar Wilde`,
+  },
+  {
+    quote: `Life is either a daring adventure or nothing at all.`,
+    author: `Helen Keller`,
+  },
+  {
+    quote: `The purpose of life is not to be happy. It is to be useful, to be honorable, to be compassionate, to have it make some difference that you have lived and lived well.`,
+    author: `Ralph Waldo Emerson`,
+  },
+  {
+    quote: `Don't count the days, make the days count.`,
+    author: `Muhammad Ali`,
+  },
+  {
+    quote: `Life is really simple, but we insist on making it complicated.`,
+    author: `Confucius`,
+  },
+  {
+    quote: `In the end, it's not the years in your life that count. It's the life in your years.`,
+    author: `Abraham Lincoln`,
+  },
+  {
+    quote: `Life is a succession of lessons which must be lived to be understood.`,
+    author: `Ralph Waldo Emerson`,
+  },
+]
+
+export default quoteList
