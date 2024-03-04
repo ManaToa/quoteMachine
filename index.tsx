e5a54192044f1520471bd5d473aa35d4421ece4e
Manatoa <43567527+ManaToa@users.noreply.github.com>
2024-03-04 19:28:08 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,46 +1,7 @@
 import { Provider, useDispatch, useSelector } from 'react-redux'
-import { configureStore, createSlice } from '@reduxjs/toolkit'
 import { FaQuoteLeft, FaXTwitter } from 'react-icons/fa6'
 import { FaTumblr } from 'react-icons/fa'
-
-import quoteList, { QuoteType } from './quotes'
-
-function getRandomQuote() {
-  return quoteList[Math.floor(Math.random() * quoteList.length)]
-}
-
-function getRandomColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`
-}
-
-interface QuoteState extends QuoteType {
-  color: string
-}
-
-const initialQuote = getRandomQuote()
-
-const quoteSlice = createSlice({
-  name: 'quote-generator',
-  initialState: {
-    quote: initialQuote.quote,
-    author: initialQuote.author,
-    color: getRandomColor(),
-  } as QuoteState,
-  reducers: {
-    updateQuoteData: state => {
-      const { quote, author } = getRandomQuote()
-      state.quote = quote
-      state.author = author
-      state.color = getRandomColor()
-    },
-  },
-})
-
-const { updateQuoteData } = quoteSlice.actions
-
-const store = configureStore({
-  reducer: quoteSlice.reducer,
-})
+import { updateQuoteData, store, QuoteState } from './redux'
 
 interface QuoteButtonProps {
   label?: string
